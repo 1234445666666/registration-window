eaba54f00afa9a0535aa3ae0f80f571c0db73d42
Your Name <you@example.com>
2025-02-04 23:52:54 +0300

    add:table and fix bags

--- js/main.js
+++ js/main.js
@@ -6,52 +6,51 @@ const userOpenPassword = document.getElementById("enterPassword");
 const userOpenLogin = document.getElementById("enterLogin");
 
 button.addEventListener("click", () => {
-    if (
-        userOpenPassword.value === userCheckpassword.value &&
-        userOpenLogin.value === userCheckLogin.value
-    ) {
-        document.querySelector(".registration").style.display = "none";
-        document.querySelector(".password").style.display = "none";
-        document.querySelector(".users__base").style.display = "none";
-        document.querySelector(".to__do").style.display = "flex";
-    } else {
-        alert("Неправильный логин или пароль");
-    }
+  if (
+    userOpenPassword.value === userCheckpassword.value &&
+    userOpenLogin.value === userCheckLogin.value
+  ) {
+    document.querySelector(".registration").style.display = "none";
+    document.querySelector(".password").style.display = "none";
+    document.querySelector(".users__base").style.display = "none";
+    document.querySelector(".to__do").style.display = "flex";
+  } else {
+    alert("Неправильный логин или пароль");
+  }
 });
 
 //скрытие поля регистации
 const btnRegistration = document.querySelector(".registration__btn");
 btnRegistration.addEventListener("click", () => {
-    if (userOpenLogin.value !== "" && userOpenPassword.value !== "") {
-        document.querySelector(".registration").style.display = "none";
-        document.querySelector(".password").style.display = "flex";
-        document.querySelector(".users__base").style.display = "none";
-    } else {
-        alert("Введите логин и пароль");
-    }
+  if (userOpenLogin.value !== "" && userOpenPassword.value !== "") {
+    document.querySelector(".registration").style.display = "none";
+    document.querySelector(".password").style.display = "flex";
+    document.querySelector(".users__base").style.display = "none";
+  } else {
+    alert("Введите логин и пароль");
+  }
 });
 
 //вернуться назад
 const btnBack = document.querySelector("#btnBack");
 btnBack.addEventListener("click", () => {
-    document.querySelector(".registration").style.display = "flex";
-    document.querySelector(".password").style.display = "none";
-    document.querySelector(".users__base").style.display = "none";
+  document.querySelector(".registration").style.display = "flex";
+  document.querySelector(".password").style.display = "none";
+  document.querySelector(".users__base").style.display = "none";
 });
 
 //показа пароля
 const passwordOpenUser = document.querySelector(".password__control");
 
-passwordOpenUser.addEventListener('click', () => {
-    if (userCheckpassword.type === 'password') {
-        userCheckpassword.type = 'text';
-        passwordOpenUser.textContent = 'скрыть пароль';
-    } else {
-        userCheckpassword.type = 'password';
-        passwordOpenUser.textContent = 'показать пароль';
-    }
-})
-
+passwordOpenUser.addEventListener("click", () => {
+  if (userCheckpassword.type === "password") {
+    userCheckpassword.type = "text";
+    passwordOpenUser.textContent = "скрыть пароль";
+  } else {
+    userCheckpassword.type = "password";
+    passwordOpenUser.textContent = "показать пароль";
+  }
+});
 
 //показ пользователей и их пароли
 const userBase = document.querySelector("#users__base");
@@ -61,30 +60,75 @@ const userPassword = document.createElement("p");
 const btnUsers = document.querySelector("#btnUsers");
 
 btnUsers.addEventListener("click", () => {
-    if (
-        userOpenPassword.value === userCheckpassword.value &&
-        userOpenLogin.value === userCheckLogin.value
-    ) {
-        document.querySelector(".registration").style.display = "none";
-        document.querySelector(".password").style.display = "none";
-        document.querySelector(".users__base").style.display = "flex";
-        userLogin.textContent =
-            `Пользователь ${userOpenLogin.value} :` +
-            "Логин: " +
-            userOpenLogin.value +
-            " Пароль: " +
-            userOpenPassword.value;
-        usersList.append(userLogin, userPassword);
-    } else {
-        alert("Чтобы посмотреть введите ваш логин и пароль");
-    }
+  if (
+    userOpenPassword.value === userCheckpassword.value &&
+    userOpenLogin.value === userCheckLogin.value
+  ) {
+    document.querySelector(".registration").style.display = "none";
+    document.querySelector(".password").style.display = "none";
+    document.querySelector(".users__base").style.display = "flex";
+    const userID = "ID-" + userOpenLogin.value;
+    userLogin.textContent =
+      `${userID} ` +
+      "Логин: " +
+      userOpenLogin.value +
+      " Пароль: " +
+      userOpenPassword.value;
+    usersList.append(userLogin, userPassword);
+  } else {
+    alert("Чтобы посмотреть введите ваш логин и пароль");
+  }
+});
+
+//табличная версия и списочная
+
+const listBtn = document.querySelector(".list__btn");
+const tableBtn = document.querySelector(".table__btn");
+
+tableBtn.addEventListener("click", () => {
+  document.querySelector("#users__base__list").style.display = "none";
+  document.querySelector("#users__base__table").style.display = "flex";
+  userLogin.style.display = "none";
+
+  tableBtn.style.display = "none";
+  listBtn.style.display = "block";
+
+  document.querySelector("#users__base__table").append(userLogin, userPassword);
+
+  const table = document.querySelector("table");
+  if (table.childElementCount === 1) {
+    const tr = document.createElement("tr");
+    const th = document.createElement("th");
+    const td = document.createElement("td");
+    th.textContent = userOpenLogin.value;
+    td.textContent = userOpenPassword.value;
+    tr.append(th, td);
+    table.append(tr);
+  } else {
+    const th = tr.querySelector("th");
+    const td = tr.querySelector("td");
+
+    th.textContent = userOpenLogin.value;
+    td.textContent = userOpenPassword.value;
+  }
+});
+
+listBtn.addEventListener("click", () => {
+  document.querySelector("#users__base__table").style.display = "none";
+  document.querySelector("#users__base__list").style.display = "flex";
+  userLogin.style.display = "flex";
+
+  tableBtn.style.display = "block";
+  listBtn.style.display = "none";
+
+  document.querySelector("#users__base__list").append(userLogin, userPassword);
 });
 
 //вернуться назад к проверке данных пользователя
 const btnBack2 = document.querySelector("#btnBack2");
 
 btnBack2.addEventListener("click", () => {
-    document.querySelector(".registration").style.display = "none";
-    document.querySelector(".password").style.display = "flex";
-    document.querySelector(".users__base").style.display = "none";
+  document.querySelector(".registration").style.display = "none";
+  document.querySelector(".password").style.display = "flex";
+  document.querySelector(".users__base").style.display = "none";
 });
--- js/to-do.js
+++ js/to-do.js
@@ -7,38 +7,36 @@ const removeToDo = document.querySelector("#remove__to__do");
 const removeAll = document.querySelector("#remove__all__to__do");
 
 addToDo.addEventListener("click", () => {
-  if (toDoInput.value !== "") {
-    const toDoItem = document.createElement("li");
-    toDoItem.textContent = toDoInput.value;
-    toDoList.append(toDoItem);
-    toDoInput.value = "";
-  } else {
-    alert("Введите задачу");
-  }
+    if (toDoInput.value !== "") {
+        const toDoItem = document.createElement("li");
+        toDoItem.textContent = toDoInput.value;
+        toDoList.append(toDoItem);
+        toDoInput.value = "";
+    } else {
+        alert("Введите задачу");
+    }
 });
 
 removeToDo.addEventListener("click", () => {
-  toDoList.lastElementChild.remove();
+    toDoList.lastElementChild.remove();
 });
 
 removeAll.addEventListener("click", () => {
-  toDoList.innerHTML = "";
+    toDoList.innerHTML = "";
 });
 
 //возвращение назад
 const btnBack3 = document.querySelector("#btnBack3");
 
 btnBack3.addEventListener("click", () => {
-  document.querySelector(".registration").style.display = "none";
-  document.querySelector(".password").style.display = "flex";
-  document.querySelector(".users__base").style.display = "none";
-  document.querySelector(".to__do").style.display = "none";
+    document.querySelector(".registration").style.display = "none";
+    document.querySelector(".password").style.display = "flex";
+    document.querySelector(".users__base").style.display = "none";
+    document.querySelector(".to__do").style.display = "none";
 });
 
 toDoList.addEventListener("click", (event) => {
-  if (event.target.tagName === "LI") {
-    event.target.classList.toggle("done");
-  }
+    if (event.target.tagName === "LI") {
+        event.target.classList.toggle("done");
+    }
 });
-
-
